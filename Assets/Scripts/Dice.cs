6de0f288094d143c929c6f9cbf268e51b04a132b
Honey Raut <hraut.au@gmail.com>
2025-08-09 18:54:52 +1000

    some gameplay loop started

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
+using TMPro;
 
 public class Dice : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private GameObject playedPanel;
-    void Start()
-    {
-        playedPanel = GameObject.Find("PlayerPlayed");
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   private GameObject playedPanel;
+   public int diceValue;
+   public bool isEnemy = false;
+   void Start()
+   {
+      if (isEnemy)
+      {
+         playedPanel = GameObject.Find("EnemyPlayed");
+      }
+      else
+      {
+         playedPanel = GameObject.Find("PlayerPlayed");
+      }
+      diceValue = Random.Range(1, 7);
+      transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = diceValue.ToString();
 
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+   }
 
-    }
+   // Update is called once per frame
+   void Update()
+   {
 
-    public void ButtonPressed()
-    {
-        transform.parent = playedPanel.transform;
-    }
+   }
+
+   public void ButtonPressed()
+   {
+      transform.parent = playedPanel.transform;
+   }
 }
--- Assets/Scripts/GameMan.cs
+++ Assets/Scripts/GameMan.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class GameMan : MonoBehaviour
 {
+    // game manager handles game states??
+    private bool playerTurn = false;
+    private int playerPts = 0;
+    private int opponentPts = 0;
+
+    // depending on how many games we are expecting to play
+    private int currGameNumber = 1;
+
+    public GameObject dicePrefab;
+    public GameObject playerSlot;
+    public GameObject playedPanel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        playedPanel = GameObject.Find("PlayerPlayed");
+        playerSlot = GameObject.Find("PlayerSlot");
+        NewGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    // game is the whole thing, so first to 12 points win the game
+    // match is the actual thing where you start with all five of your dice and play it
+    // 
+    public void NewGame()
+    {
+        // maybe begins a new scene could be some animations to signify it being a new game  
+        playerPts = 0;
+        opponentPts = 0;
+        NewMatch();
+    }
+    public void NewMatch()
+    {
+        // return dice to each player
+        // randomly assign values to each of the die
+        foreach (Transform child in playedPanel.transform) Destroy(child.gameObject);
+        foreach (Transform child in playerSlot.transform) Destroy(child.gameObject);
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject die = Instantiate(dicePrefab, Vector3.zero, Quaternion.identity);
+            die.transform.parent = playerSlot.transform;
+        }
+
+        playerTurn = false;
+
+    } 
+
+ 
 }
