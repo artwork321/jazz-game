224a3070fdf8dba92961d42088380da3ddfc9214
Honey Raut <hraut.au@gmail.com>
2025-08-09 17:15:49 +1000

    dice pressed

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class Dice : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private GameObject playedPanel;
     void Start()
     {
-        
+        playedPanel = GameObject.Find("PlayerPlayed");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ButtonPressed()
+    {
+        transform.parent = playedPanel.transform;
     }
 }
