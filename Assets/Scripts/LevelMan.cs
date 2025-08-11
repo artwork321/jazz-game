2afd1cd71fb73406117b9b9455122489fd56f885
Honey Raut <hraut.au@gmail.com>
2025-08-11 12:54:40 +1000

    level man

--- Assets/Scripts/LevelMan.cs
+++ Assets/Scripts/LevelMan.cs
@@ -1,16 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelMan : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Level> levels;
+    private int currentLevelIndex = -1; 
+
+    public void StartNewLevel(int levelIndex)
     {
-        
+        if (levelIndex < 0 || levelIndex >= levels.Count)
+        {
+            // either we start boss level -> maybe we put this on a new scene or smth im not sure
+            // or this will trigger the end of the game maybe.
+            return;
+        }
+
+        currentLevelIndex = levelIndex;
+        Level level = levels[levelIndex];
+
+        // Example: Play music for this level
+        if (level.bgMusic != null)
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource != null)
+            {
+                audioSource.clip = level.bgMusic;
+                audioSource.Play();
+            }
+        }
+
+        // Trigger dialogue
+        FindObjectOfType<Dialogue>().PlayLine(level.dialogueIndex);
+
+        Debug.Log("Started Level " + level.lvl);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToNextLevel()
     {
-        
+        StartNewLevel(currentLevelIndex + 1);
+    }
+
+    public Level GetCurrentLevel()
+    {
+        if (currentLevelIndex >= 0 && currentLevelIndex < levels.Count)
+            return levels[currentLevelIndex];
+        return null;
     }
 }
+
+[System.Serializable]
+public class Level
+{
+    public int lvl;            // level number
+    public int dialogueIndex;  // starting dialogue index
+    public AudioClip bgMusic;  // background music
+}
+
